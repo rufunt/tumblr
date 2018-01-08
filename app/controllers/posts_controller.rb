1368b8952dbbfc15e38b99002d709b43a44485e4
rufunt <jazzoo@mail.ru>
2018-01-09 02:16:38 +0300

    update and save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     
     if @post.save
-    redirect_to @post
+      redirect_to @post
     else
       render 'new'
     end
